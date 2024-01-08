bd08ed1ef7e47a5b4eabb0dbe868e96e37ca3e7b
ebcode2021 <ebcode2021@gmail.com>
2024-01-08 16:53:46 +0900

    [add] OrderServiceImplTest

--- core/src/test/java/hello/core/order/OrderServiceImplTest.java
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -7,7 +7,6 @@ import hello.core.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
 

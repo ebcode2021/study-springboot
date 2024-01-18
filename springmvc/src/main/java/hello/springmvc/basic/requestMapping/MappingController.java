ac591720aff76ff9632df1356a338da71348857a
ebcode2021 <ebcode2021@gmail.com>
2024-01-18 17:28:32 +0900

    [update] 오타 확인 후 삭제

--- springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingController.java
@@ -1,11 +1,9 @@
-기package hello.springmvc.basic.requestMapping;
+package hello.springmvc.basic.requestMapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
-import java.lang.invoke.MethodType;
 
 @RestController
 public class MappingController {
